6c635090d92608829c2f13acea9a6da9d2b3984a
lap-does-things <laptokgaming7@gmail.com>
2025-04-18 23:00:46 +0300

    we made casino family-friendly

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import random
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
-@app.route('/nigger/cock')
-def nigger():
+@app.route('/casino/krutka')
+def casino():
     mult = request.args.get('mult')
+    stake = request.args.get('stake')
+    stake = int(stake)
+    mult = int(mult)
     магическоерандомноечисло = random.randint(1,6)
     if магическоерандомноечисло <= int(mult):
-        return jsonify(result={'win' : 'nenihuya'})
-    else :return jsonify(result={'win' : 'huya'})
+        return jsonify(result={'win' : False, 'stake' : int(stake*mult)})
+    else :return jsonify(result={'win' :  True, 'stake' : int(stake*mult)})
+
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,57 @@
 let currentMultiplier = 1;
 let rotation = 0;
+btn1 = document.getElementById("btn1");
+btn2 = document.getElementById("btn2");
+btn3 = document.getElementById("btn3");
+btn4 = document.getElementById("btn4");
+btn5 = document.getElementById("btn5");
 
 function setMultiplier(x) {
-        document.getElementById("btn1").classList.remove("green");
-        document.getElementById("btn2").classList.remove("green");
-        document.getElementById("btn3").classList.remove("green");
-        document.getElementById("btn4").classList.remove("green");
-        document.getElementById("btn5").classList.remove("green");
-        document.getElementById("btn1").classList.remove("red");
-        document.getElementById("btn2").classList.remove("red");
-        document.getElementById("btn3").classList.remove("red");
-        document.getElementById("btn4").classList.remove("red");
-        document.getElementById("btn5").classList.remove("red");
+        btn1.classList.remove("green");
+        btn2.classList.remove("green");
+        btn3.classList.remove("green");
+        btn4.classList.remove("green");
+        btn5.classList.remove("green");
+        btn1.classList.remove("red");
+        btn2.classList.remove("red");
+        btn3.classList.remove("red");
+        btn4.classList.remove("red");
+        btn5.classList.remove("red");
     switch (x) {
         case 1:
-            document.getElementById("btn1").classList.add("green");
-            document.getElementById("btn2").classList.add("red");
-            document.getElementById("btn3").classList.add("red");
-            document.getElementById("btn4").classList.add("red");
-            document.getElementById("btn5").classList.add("red");
+            btn1.classList.add("green");
+            btn2.classList.add("red");
+            btn3.classList.add("red");
+            btn4.classList.add("red");
+            btn5.classList.add("red");
             break;
         case 2:
-            document.getElementById("btn2").classList.add("green");
-            document.getElementById("btn1").classList.add("red");
-            document.getElementById("btn3").classList.add("red");
-            document.getElementById("btn4").classList.add("red");
-            document.getElementById("btn5").classList.add("red");
+            btn2.classList.add("green");
+            btn1.classList.add("red");
+            btn3.classList.add("red");
+            btn4.classList.add("red");
+            btn5.classList.add("red");
             break;
         case 3:
-            document.getElementById("btn3").classList.add("green");
-            document.getElementById("btn2").classList.add("red");
-            document.getElementById("btn1").classList.add("red");
-            document.getElementById("btn4").classList.add("red");
-            document.getElementById("btn5").classList.add("red");
+            btn3.classList.add("green");
+            btn2.classList.add("red");
+            btn1.classList.add("red");
+            btn4.classList.add("red");
+            btn5.classList.add("red");
             break;
         case 4:
-            document.getElementById("btn4").classList.add("green");
-            document.getElementById("btn2").classList.add("red");
-            document.getElementById("btn3").classList.add("red");
-            document.getElementById("btn1").classList.add("red");
-            document.getElementById("btn5").classList.add("red");
+            btn4.classList.add("green");
+            btn2.classList.add("red");
+            btn3.classList.add("red");
+            btn1.classList.add("red");
+            btn5.classList.add("red");
             break;
         case 5:
-            document.getElementById("btn5").classList.add("green");
-            document.getElementById("btn2").classList.add("red");
-            document.getElementById("btn3").classList.add("red");
-            document.getElementById("btn4").classList.add("red");
-            document.getElementById("btn1").classList.add("red");
+            btn5.classList.add("green");
+            btn2.classList.add("red");
+            btn3.classList.add("red");
+            btn4.classList.add("red");
+            btn1.classList.add("red");
             break;
     }
     currentMultiplier = x;
@@ -55,13 +60,15 @@ function setMultiplier(x) {
 }
 
 document.querySelector('.submit-btn').addEventListener('click', () => {
-    $.getJSON(`/nigger/cock?mult=${currentMultiplier}`,
-        function(data, textStatus, jqXHR) {
+    const StakeField = document.getElementById("stake");
+    $.getJSON(`/casino/krutka?mult=${currentMultiplier}&stake=${StakeField.value}`,
+        function(data) {
             win = data.result.win;
+            stake = data.result.stake;
         }
     );
-    const niggerbtn = document.getElementById("niggerbtn");
-    niggerbtn.style.display = "none";
+    const casinobtn = document.getElementById("casinobtn");
+    casinobtn.style.display = "none";
     const img = document.getElementById("cylinder");
 
     // Генерируем случайный поворот: 3–5 оборотов
@@ -78,18 +85,24 @@ document.querySelector('.submit-btn').addEventListener('click', () => {
     setTimeout(() => {
 
         const resultDiv = document.getElementById("resultMessage");
-        if (win == 'nenihuya') {
-            resultDiv.innerText = "Вы проиграли!";
+        if (!win) {
+            resultDiv.innerText = `Вы проиграли! Вы лишились ${stake} рублей!`;
+            StakeField.value = Number(StakeField.value) - stake; // TODO : ЗАМЕНИТЬ VALUE НА ОБЩИЙ СЧЕТ ЮЗНИ
+            if (StakeField.value < stake) {
+                resultDiv.innerText = `Вы проиграли! Вы безбожно лишились ${stake} рублей!`;
+                StakeField.value = 0;
+            }
         } else {
-            resultDiv.innerText = "Вы победили!";
+            resultDiv.innerText = `Вы победили!  Вы получили ${stake} рублей!`;
+            StakeField.value = Number(StakeField.value) + stake;
         }
         resultDiv.style.display = "flex";
-        // Я ТРАХАЛ ВОЛА XDDDDD
+
         // Убрать через 3 секунды
         setTimeout(() => {
             resultDiv.style.display = "none";
             //resultDiv.remove();
-            niggerbtn.style.display = "inline-block";
+            casinobtn.style.display = "inline-block";
         }, 3000);
     }, 2100);
 });
