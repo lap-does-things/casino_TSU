32b33b8113240b30a61e1258fd2dc161d5081e0a
lap-does-things <laptokgaming7@gmail.com>
2025-04-19 17:01:46 +0300

    йфцы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import random, MySQLdb.cursors, re, flask_mysqldb
 
 app = Flask(__name__)
@@ -10,6 +10,22 @@ app.config['MYSQL_DB'] = 'russian_roulette'
 
 mysql = flask_mysqldb.MySQL(app)
 
+@app.route('/user/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return render_template('index.html')
+#TODO : остановился тут. регу и логин сделать через дб
+def check_login(username, password):
+    with mysql.connection() as conn:
+        with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cur:
+            cur.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+            result = cur.fetchone()
+            if result:
+                return True
+            else:
+                return False
 
 @app.route('/')
 def home():
