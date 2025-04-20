616ab0fe7e3986ee691856ab4b02673a97b5b066
lap-does-things <laptokgaming7@gmail.com>
2025-04-20 13:23:08 +0300

    dont act like it aint yeezy

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,7 @@ btn2 = document.getElementById("btn2");
 btn3 = document.getElementById("btn3");
 btn4 = document.getElementById("btn4");
 btn5 = document.getElementById("btn5");
+var win;
 
 function setMultiplier(x) {
         btn1.classList.remove("green");
@@ -77,65 +78,133 @@ document.querySelector('.submit-btn').addEventListener('click', () => {
         function(data) {
             win = data.result.win;
             stake = data.result.stake;
-        }
-    );
-    const casinobtn = document.getElementById("casinobtn");
-    casinobtn.style.display = "none";
-    const img = document.getElementById("cylinder");
-
-    // Генерируем случайный поворот: 3–5 оборотов
-    // [x] это ХУЙНЯ!!!!!!!!!!!!!!!!!
-    /*
-    Обороты никак не связаны с количеством и расположением патронов в барабане.
-    Это просто случайный поворот цилиндра, и часто выпадает пуля, когда ты победил и наоборот.
-
-    Я ТРАХАЛ ТОГО КТО ЭТО ПИСАЛ
-
-(если это не мужик)
-
--Лэп.
-
-P.S. если это правда не мужик, то я серьёзно ;)
-    */
-
-// https://a.d-cd.net/R34aG_8UGYblYQsYHOgDR9h9jMA-960.jpg
-
-// Почему дальше по коду он видит win в скоупе, а тут нет????
-    var turns = Math.floor(Math.random() * 5) + 11;
-//    alert(win);
-    //Какого-то хуя не работают ни if, ни switch. Хуйня. Хуйня. Хуйня. Хуйня. Хуйня. Просто виснет скрипт
-    // В конце кода он видит win в скоупе, а тут нет????
-    rotation += 360 * turns;
-
-    img.style.transform = `rotate(${rotation}deg)`;
-
-    img.classList.remove("rotate"); // сброс, если уже был запуск
-    void img.offsetWidth; // форс пересчёт layout — для повторной анимации
-    img.classList.add("rotate");
-
-    // Через 2.1 секунды — когда закончится анимация — показываем результат
-    setTimeout(() => {
-
-        const resultDiv = document.getElementById("resultMessage");
-        if (!win == true) {
-            resultDiv.innerText = `Вы проиграли! Вы лишились ${stake} рублей!`;
-            StakeField.value = Number(StakeField.value) - stake; // TODO : ЗАМЕНИТЬ VALUE НА ОБЩИЙ СЧЕТ ЮЗНИ
-            if (StakeField.value < stake) {
-                resultDiv.innerText = `Вы проиграли! Вы безбожно лишились ${stake} рублей!`;
-                StakeField.value = 0;
+            win = false; //[x] DELETEME  DEBUG
+            switch (win) {
+                case true:
+                    //alert("Вы победили!  Вы получили " + stake + " рублей!");
+                    switch(currentMultiplier) {
+                        case 1:
+                            alert("1,1");
+                            turns = 3;
+                            break;
+                        case 2:
+                            alert("Вы победили!  Вы получили " + stake + " рублей!");
+                            break;
+                        case 3:
+                            alert("Вы победили!  Вы получили " + stake + " рублей!");
+                            break;
+                        case 4:
+                            alert("Вы победили!  Вы получили " + stake + " рублей!");
+                            break;
+                        case 5:
+                            //alert("Вы победили!  Вы получили " + stake + " рублей!");
+                            break;
+                        default:
+                            alert("default");
+                            break;
+                    }
+                    break;
+                case false:
+                    switch(currentMultiplier) {
+                        case 1:
+                            alert("2,1");
+                            turns = 2.8;
+                            break;
+                        case 2:
+                            alert("Вы проиграли! Вы лишились " + stake + " рублей!");
+                            break;
+                        case 3:
+                            alert("Вы проиграли! Вы лишились " + stake + " рублей!");
+                            break;
+                        case 4:
+                            alert("Вы проиграли! Вы лишились " + stake + " рублей!");
+                            break;
+                        case 5:
+                            alert("Вы проиграли! Вы лишились " + stake + " рублей!");
+                            // Вы проиграли! Вы лишились 10 рублей!
+                            break;
+                        default:
+                            alert("default");
+                            break;
+                    }
+                    break;
+                default:
+                    alert("default");
+                    break;
             }
-        } else {
-            resultDiv.innerText = `Вы победили!  Вы получили ${stake} рублей!`;
-            StakeField.value = Number(StakeField.value) + stake;
-        }
-        resultDiv.style.display = "flex";
+            const casinobtn = document.getElementById("casinobtn");
+            casinobtn.style.display = "none";
+            const img = document.getElementById("cylinder");
+            win = win;
+            stake = stake;
+            var turns = turns;
 
-        // Убрать через 3 секунды
-        setTimeout(() => {
-            resultDiv.style.display = "none";
-            //resultDiv.remove();
-            casinobtn.style.display = "inline-block";
-        }, 3000);
-    }, 2100);
+            // Генерируем случайный поворот: 3–5 оборотов
+            // [x] это ХУЙНЯ!!!!!!!!!!!!!!!!!
+            /*
+            Обороты никак не связаны с количеством и расположением патронов в барабане.
+            Это просто случайный поворот цилиндра, и часто выпадает пуля, когда ты победил и наоборот.
+
+            Я ТРАХАЛ ТОГО КТО ЭТО ПИСАЛ
+
+        (если это не мужик)
+
+        -Лэп.
+
+        P.S. если это правда не мужик, то я серьёзно ;)
+            */
+
+        // https://a.d-cd.net/R34aG_8UGYblYQsYHOgDR9h9jMA-960.jpg
+
+        // Почему дальше по коду он видит win в скоупе, а тут нет????
+            //turns = Math.floor(Math.random() * 5) + 11;
+
+            //Какого-то хуя не работают ни if, ни switch. Хуйня. Хуйня. Хуйня. Хуйня. Хуйня. Просто виснет скрипт
+            // В конце кода он видит win в скоупе, а тут нет????
+            rotation += 360 * turns;
+            img.classList.remove("rotate");
+            img.style.transform = `rotate(${rotation}deg)`;
+
+            img.classList.remove("rotate"); // сброс, если уже был запуск
+            void img.offsetWidth; // форс пересчёт layout — для повторной анимации
+            img.classList.add("rotate");
+
+            // Через 2.1 секунды — когда закончится анимация — показываем результат
+            setTimeout(() => {
+
+                const resultDiv = document.getElementById("resultMessage");
+                if (!win == true) {
+                    resultDiv.innerText = `Вы проиграли! Вы лишились ${stake} рублей!`;
+                    StakeField.value = Number(StakeField.value) - stake; // TODO : ЗАМЕНИТЬ VALUE НА ОБЩИЙ СЧЕТ ЮЗНИ
+                    if (StakeField.value < stake) {
+                        resultDiv.innerText = `Вы проиграли! Вы безбожно лишились ${stake} рублей!`;
+                        StakeField.value = 0;
+                    }
+                } else {
+                    resultDiv.innerText = `Вы победили!  Вы получили ${stake} рублей!`;
+                    StakeField.value = Number(StakeField.value) + stake;
+                }
+                resultDiv.style.display = "flex";
+
+                // Убрать через 3 секунды
+                setTimeout(() => {
+                    resultDiv.style.display = "none";
+                    casinobtn.style.display = "inline-block";
+                    img.style.transform = `rotate(0deg)`; // !!!! ВАЖНО !!! это сброс анимации, чтобы углы сверху работали
+                    // !!!!! Если ты их уберешь, будет рассинхрон в анимации и исходе крутки !!!!
+                    // !!!! да и вообще выглядит пиздато тоже !!!!
+                }, 3000);
+            }, 2100);
+            img.classList.remove("rotate"); // сброс, если уже был запуск
+            void img.offsetWidth; // форс пересчёт layout — для повторной анимации
+            img.classList.add("rotate");
+
+            turns = 0;
+            return turns;
+        }
+
+    );
+
+
     // Люблю вас, ребята - Лэп.
 });
